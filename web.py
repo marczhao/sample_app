58e7d4be0a69a9c3605ea1092b037434e1869baa
Marc Zhao <szhao17@gsb.columbia.edu>
2016-10-17 20:51:26 -0400

    Created stock look up

--- web.py
+++ web.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template
+from googlefinance import getQuotes
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+def get_stock_price(ticker):
+	quotes = getQuotes(ticker)
+	price = quotes[0]['LastTradePrice']
+	return "The price of {} is {}".format(ticker, price)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	name = request.values.get('name')
+	greeting = "您好 {}".format(name)
+	return render_template('index.html', greeting=greeting)
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+@app.route('/results')
+def results():
+	stock = request.values.get('stock')
+	price = get_stock_price(stock)
+	return render_template('results.html', price=price)
+
+# @app.route('/in/<username>')
+# def profile():
+# 	user = get_user(username)
+# 	return render_template('profile.html', user=user)
 
 app.run(debug=True)
